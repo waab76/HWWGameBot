4d7261ae8bd7ca6092dc26c09b8fd8c053674e7f
Rob Curtis <waab@me.com>
2023-07-12 21:30:33 -0400

    Updates toward a more functional bot

--- periodical.py
+++ periodical.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta, timezone
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import Config
@@ -7,19 +7,102 @@ handlers = set()
 handlers.add(TimedRotatingFileHandler('HWWGameBot.log', when='W0', backupCount=4))
 
 logging.basicConfig(level=logging.INFO, handlers=handlers, format='%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s')
-logging.Formatter.formatTime = (lambda self, record, datefmt=None: datetime.datetime.fromtimestamp(record.created, datetime.timezone.utc).astimezone().isoformat(sep="T",timespec="milliseconds"))
+logging.Formatter.formatTime = (lambda self, record, datefmt=None: datetime.fromtimestamp(record.created, timezone.utc).astimezone().isoformat(sep="T",timespec="milliseconds"))
 
 import praw
+import requests
 
-request_url = "http://0.0.0.0:8000"
-check_time = datetime.datetime.utcnow().time()
+request_url = 'http://0.0.0.0:8800'
+check_time = datetime.now(timezone.utc)
+
+def get_active_phase(reddit):
+    payload = requests.get(request_url + '/active-phase/').json()
+    logging.debug('Active phase post is {}'.format(payload['post_id']))
+    return reddit.submission(payload['post_id'])
+
+def set_active_phase(submission):
+    logging.debug('Updating active phase post to [{}]'.format(submission.id))
+    requests.post(request_url + '/active-phase/', {'post_id': submission.id})
+
+def get_phase_length():
+    return timedelta(hours=1)
+
+def process_votes(submission):
+    logging.debug('Processing votes for phase [{}]'.format(submission.id))
+
+    # Get comments from submission in chronological order
+
+    # for each comment
+        # if not already seen
+            # if it's a vote
+                # add vote to votes payload
+            # mark comment as seen
+
+    # submit votes payload to server
+
+def process_actions(reddit):
+    logging.debug('Processing actions for current phase')
+
+    # Get unread messages from inbox
+
+    # for each message
+        # if it is an action
+            # add action to the actions payload
+        # mark it as read
+
+    # submit actions payload to the server
+
+def handle_turnover(submission, subreddit, reddit):
+    logging.info('Handling turnover for phase [{}]'.format(submission.id))
+
+    # Lock the post
+    logging.debug('Locking phase post [{}]'.format(submission.id))
+    try:
+        submission.mod.lock()
+    except:
+        logging.exception('Failed to lock phase post [{}]'.format(submission.id))
+
+    # Process any final comments
+    logging.debug('Processing final votes from phase post [{}]'.format(submission.id))
+    process_votes(submission)
+
+    # Process any final action submissions
+    logging.debug('Processing final action submissions for phase [{}]'.format(submission.id))
+    process_actions(reddit)
+
+    # Get new phase title and text from server
+    logging.debug('Handling turnover on server')
+    new_phase_data = requests.get(request_url + '/handle_turnover/').json()
+    logging.debug('New phase data from server:\n{}\n\n{}'.format(new_phase_data['title'], new_phase_data['body']))
+
+    # Create a new phase post
+    new_phase = subreddit.submit(title=new_phase_data['title'], selftext=new_phase_data['body'], send_replies=False)
+    logging.debug('Posted new phase [{}]'.format(new_phase.id))
+
+    # Store the new phase post id
+    set_active_phase(new_phase)
+
 
 def main():
     config = Config.Config('myconfig')
     reddit = config.reddit_object
-    sub = config.subreddit_object
+    subreddit = config.subreddit_object
 
-    logging.info('Logged into Reddit as {}'.format(reddit.user.me()))
+    # Get the active game post
+    active_game_post = get_active_phase(reddit)
 
-if __name__ == "__main__":
+    # Check whether or not it's time for turnover
+    post_time = datetime.fromtimestamp(active_game_post.created_utc, timezone.utc)
+    if check_time - post_time > get_phase_length():
+        logging.debug('Time since phase was posted {}'.format(check_time - post_time))
+        handle_turnover(active_game_post, subreddit, reddit)
+    else:
+        logging.debug('{} to turnover'.format(get_phase_length() - (check_time - post_time)))
+        # Process any votes
+        process_votes(active_game_post)
+
+        # Process any action submissions
+        process_actions(reddit)
+
+if __name__ == '__main__':
 	main()
--- server.py
+++ server.py
@@ -3,16 +3,14 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 handlers = set()
-handlers.add(TimedRotatingFileHandler('HWWGameBotServer.log',
-									  when='W0',
-									  backupCount=4))
-logging.basicConfig(level=logging.DEBUG, handlers=handlers,
-					format='%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s')
+handlers.add(TimedRotatingFileHandler('HWWGameBot.log', when='W0', backupCount=4))
+logging.basicConfig(level=logging.INFO, handlers=handlers, format='%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s')
 logging.Formatter.formatTime = (lambda self, record, datefmt=None: datetime.datetime.fromtimestamp(record.created, datetime.timezone.utc).astimezone().isoformat(sep="T",timespec="milliseconds"))
 
+import json
 import os
 import sys
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 from werkzeug.serving import WSGIRequestHandler
 
 #https://stackoverflow.com/questions/54141751/how-to-disable-flask-app-run-s-default-message
@@ -22,79 +20,165 @@ cli.show_server_banner = lambda *x: None
 app = Flask(__name__)
 
 class JsonHelper:
-	def ascii_encode_dict(self, data):
-		ascii_encode = lambda x: x.encode('ascii') if isinstance(x, str) else x
-		return dict(map(ascii_encode, pair) for pair in data.items())
+    def ascii_encode_dict(self, data):
+        ascii_encode = lambda x: x.encode('ascii') if isinstance(x, str) else x
+        return dict(map(ascii_encode, pair) for pair in data.items())
 
-	def get_db(self, fname, encode_ascii=True):
-		with open(fname) as json_data:
-			if encode_ascii:
-				data = json.load(json_data, object_hook=self.ascii_encode_dict)
-			else:
-				data = json.load(json_data)
-		return data
+    def get_db(self, fname, encode_ascii=True):
+        with open(fname) as json_data:
+            if encode_ascii:
+                data = json.load(json_data, object_hook=self.ascii_encode_dict)
+            else:
+                data = json.load(json_data)
+        return data
 
-	def dump(self, db, fname):
-		with open(fname, 'w') as outfile:
-			outfile.write(str(db).replace("'", '"').replace('{u"', '{"').replace('[u"', '["').replace(' u"', ' "'))
+    def dump(self, db, fname):
+        with open(fname, 'w') as outfile:
+            outfile.write(str(db).replace("'", '"').replace('{u"', '{"').replace('[u"', '["').replace(' u"', ' "'))
 
 json_helper = JsonHelper()
 
 # Specify data file names
+active_game_fname = 'database/active_game.json'
+active_phase_fname = 'database/active_phase.json'
 
 # Declare data file structures
-the_chache = {}
+active_game = {}
+active_phase = {}
 
-@app.route('/get-test/', methods=["GET"])
-def test_get():
+@app.route('/active-phase/', methods=['GET', 'POST'])
+def active_game_post():
     """
-    What does this function do?
+    Handles queries/updates related to the currently active game phase post
 
     Expected Form Params:
-    None
+    String post_id: The ID of the active game post
     """
 
-    logging.info('Call to /get-test/')
-	return jsonify({'count': 4, 'foo': 'bar', 'baz': {'quick': ['brown','fox']}})
+    global active_game
+    logging.debug('Active game data: {}'.format(active_game))
 
-@app.route('/post-test/', methods=['POST'])
-def post_test():
-	"""
-	Given a user name and karma data, stores it in the cache
+    if request.method == 'GET':
+        return(jsonify({'post_id': active_game['active_post_id']}))
+    elif request.method == 'POST':
+        logging.debug('POST request')
+        active_game['active_post_id'] = request.form['post_id']
+        json_helper.dump(active_game, active_game_fname)
+        logging.debug('Active game data: {}'.format(active_game))
+        return(jsonify({'post_id': active_game['active_post_id']}))
+    else:
+        logging.error('DAFUQ request')
+        abort(500)
 
-	Requested Form Params:
-	String key: The cache key
-	String value: The cache value
-	"""
+# TODO: Add route to fetch list of live players
 
-    logging.info('Call to /post-test/')
-	cache_key = request.form['key']
-	cache_entry = json.loads(request.form['value'])
-	cache_entry['timestamp'] = int(time.time())
+# TODO: Add route for game config
 
-    global the_cache
-    the_cache[cache_key] = cache_entry
+@app.route('/actions/', methods=['POST'])
+def handle_actions():
+    """
+    Handles updates to the votes for the current phase
 
-	logging.info('Update cache for [{}] to {}'.format(cache_key, cache_entry))
+    Expected Form Params:
+    String actions: The actions to update as a JSON map of the form {actor: [target1, target2], etc}
+    """
 
-	return jsonify({'added': 1})
+    global active_game
+    global active_phase
+
+    actions = json.load(request.form['actions'])
+
+    # for actor in actions:
+        # if actor is alive
+            # if targets are alive
+                # active_phase['actions'][actor] = actions[actor]
+            # else
+                # add error to response for dead targets
+        # else
+            # add error to response for dead actor
+
+    json_helper.dump(active_phase, active_phase_fname)
+
+    # return errors to caller
+
+@app.route('/votes/', methods=['POST'])
+def handle_votes():
+    """
+    Handles updates to the votes for the current phase
+
+    Expected Form Params:
+    String votes: The votes to update as a JSON map of the form {voter1: target1, etc}
+    """
+
+    global active_game
+    global active_phase
+
+    votes = json.load(request.form['votes'])
+
+    # for voter in votes:
+        # if voter is alive
+            # if target is alive
+                # active_phase['votes'][voter] = votes[voter]
+            # else
+                # add error to response for dead target
+        # else
+            # add error to response for dead voter
+
+    json_helper.dump(active_phase, active_phase_fname)
+
+    # return errors to caller
+
+@app.route('/handle_turnover/', methods=['GET'])
+def handle_turnover():
+
+    global active_phase
+    global active_game
+
+    # Tally votes from active_phase
+    logging.debug('Tallying votes for phase {}'.format(active_game['active_post_id']))
+
+    # Handle inactivity removals
+    logging.debug('Handling inactivity removals')
+
+    # Process actions from active_phase
+    logging.debug('Handling actions')
+
+    # Generate new phase title
+    phase_title = 'New phase title'
+
+    # Generate new phase text
+    phase_body = 'Hey look, a new phase.  Someone probably got voted out. Someone else probably got killed.'
+
+    # Roll over the active phase database
+    active_phase = {'votes':{}, 'actions':{}}
+    json_helper.dump(active_phase, active_phase_fname)
+
+    return(jsonify({'title': phase_title, 'body': phase_body}))
 
 class MyRequestHandler(WSGIRequestHandler):
     def log_request(self, code='-', size='-'):
         if 200 == code:
-            logging.info('"%s" %s %s', self.requestline, code, size)
             pass
         else:
             logging.info('"%s" %s %s', self.requestline, code, size)
 
 def launch():
-	logging.info('Starting server...')
+    logging.info('Starting server...')
 
+    logging.debug('Loading active game data from {}'.format(active_game_fname))
+    global active_game
+    active_game = json_helper.get_db(active_game_fname, encode_ascii=False)
+    logging.debug('Active game data: {}'.format(active_game))
+
+    logging.debug('Loading active phase data from {}'.format(active_phase_fname))
+    global active_phase
+    active_phase = json_helper.get_db(active_phase_fname, encode_ascii=False)
+    logging.debug('Active phase data: {}'.format(active_phase))
 
 if __name__ == "__main__":
-	try:
-		launch()
-		app.run(host= '0.0.0.0', port=8000, request_handler=MyRequestHandler)
-	except Exception as e:
-		if str(e).lower() != '[Errno 98] Address already in use'.lower():
-			logging.exception(e)
+    try:
+        launch()
+        app.run(host= '0.0.0.0', port=8800, request_handler=MyRequestHandler)
+    except Exception as e:
+        if str(e).lower() != '[Errno 98] Address already in use'.lower():
+            logging.exception(e)
